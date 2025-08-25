1efeb8fe7cf0eb6979ed491e11d035aa4de5eeb4
OV <o.varlamov@globexit.ru>
2025-08-25 18:24:33 +0300

    step 9

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createElement,Fragment } from 'react' 
 
 
 // императивный
@@ -11,30 +12,21 @@ function App() {
   const year = new Date().getFullYear();
 
   return (
-    // декларативный
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">npm
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>{year}</div>
-    </>
+    // императивный
+    createElement(Fragment,null,
+      createElement('div',null, 
+      createElement('a',{href:"https://vite.dev",target:"_black"},createElement('img',{className:"logo",alt:"Vite logo",src:viteLogo})),
+      createElement('a',{href:"https://react.dev",target:"_black"},createElement('img',{className:"logo react",alt:"logo react",src:reactLogo}))
+      ),
+      createElement('h1',null,'Vite + React'),
+      createElement('div',{className:"card"},
+        'npm', 
+        createElement('button',{onClick:() => setCount((count) => count + 1)},`count is ${count}`),
+        createElement('p',null,' Edit ', createElement('code',null,'src/App.jsx'),' and save to test HMR')
+      ),
+      createElement('p',{className:"read-the-docs"},'Click on the Vite and React logos to learn more'),
+      createElement('div',null,year)
+    )
   )
 }
 
